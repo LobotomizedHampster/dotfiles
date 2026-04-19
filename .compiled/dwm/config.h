75be59974134adfba906fc9175d4b53117c39ec0
noah <imnoahmiller@gmail.com>
2026-04-19 08:03:53 -0500

    (dwm) changed refresh rate and border color

--- .compiled/dwm/config.h
+++ .compiled/dwm/config.h
@@ -5,22 +5,20 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-/*static const char *fonts[]          = { "monospace:size=10" };*/
 static const char *fonts[]          = { "Hermit:size=10" };
-/*static const char dmenufont[]       = "monospace:size=10";*/
 static const char dmenufont[]       = "Hermit:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_white[]        = "#ebdbc2";
-static const char col_black[]        = "#161414";
-static const char col_muted[]        = "#a89984";
+static const char col_white[]       = "#ebdbc2";
+static const char col_black[]       = "#161414";
+static const char col_muted[]       = "#a89984";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_white },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_black  },
 };
 
 /* tagging */
@@ -41,7 +39,7 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
+static const int refreshrate = 60;  /* refresh rate (per second) for client move/resize */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
