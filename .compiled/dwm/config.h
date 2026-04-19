c48b6505d9477da051ce6421cf6c9b8221d7b179
noah <imnoahmiller@gmail.com>
2026-04-19 08:15:52 -0500

    changed workspace hotkeys

--- .compiled/dwm/config.h
+++ .compiled/dwm/config.h
@@ -89,13 +89,13 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_z,                      0)
+	TAGKEYS(                        XK_a,                      1)
+	TAGKEYS(                        XK_x,                      2)
+	TAGKEYS(                        XK_s,                      3)
+	TAGKEYS(                        XK_d,                      4)
+	TAGKEYS(                        XK_c,                      5)
+	TAGKEYS(                        XK_f,                      6)
 	/*TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)*/
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
