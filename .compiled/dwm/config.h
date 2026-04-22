3ba3dd442fb6228645b2e00f60aaebdad386accc
noah <imnoahmiller@gmail.com>
2026-04-21 20:51:42 -0500

    (dwm) chnaged color scheme and dmenu cmd

--- .compiled/dwm/config.h
+++ .compiled/dwm/config.h
@@ -17,8 +17,8 @@ static const char col_black[]       = "#161414";
 static const char col_muted[]       = "#a89984";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_muted },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_white  },
+	[SchemeNorm] = { col_white, col_black, col_muted },
+	[SchemeSel]  = { col_black, col_white, col_white },
 };
 
 /* tagging */
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
