08068b674a19da7b275aeae06b722a0b7b36059d
noah <imnoahmiller@gmail.com>
2026-04-21 20:38:57 -0500

    (dwm) patched cfact and made doublecolumn compatable with it

--- .compiled/dwm/gaplessgrid.c
+++ .compiled/dwm/gaplessgrid.c
@@ -1,10 +1,14 @@
 void
 gaplessgrid(Monitor *m) {
-	unsigned int n, col, lrow, rrow, cn, rn, /*i,*/ row, cx, cy, cw, ch;
+	unsigned int n, col, lrow, rrow, cn, rn, /*i,*/ row, cx, cy, cw, ch, i, cyn, tyl, tyr;
+    float rfacts = 0;
+    float lfacts = 0;
 	Client *c;
 
     /* number of non-minimized and tiled windows */
-	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++) ;
+	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++); /*{
+        afacts += c->cfiact;
+    };*/
 	if(n == 0) /* cancel if 0 */
 		return;
 
@@ -25,25 +29,57 @@ gaplessgrid(Monitor *m) {
         rrow = lrow;
     }
 
+	cn = 0; /* current column number */
+	rn = 0; /* current row number */
+
+	for(i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
+        if(cn) {
+            rfacts += c->cfact;
+        } else {
+            lfacts += c->cfact;
+        }
+        rn++;
+        if((cn == 0 && rn >= lrow) || 
+           (cn == 1 && rn >= rrow)) {
+            rn = 0;
+            cn = 1;
+        }
+    }
 
 	cw = m->ww / col; /* column width */
 
 
 	cn = 0; /* current column number */
 	rn = 0; /* current row number */
-	for(/*i = 0,*/ c = nexttiled(m->clients); c; /*i++,*/ c = nexttiled(c->next)) {
+    tyl = 0; /* y pos for window placment */
+    tyr = 0; 
+	for(c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
         /* determine which column the window is in */
         if(cn) {
             row = rrow;
             cx = m->wx + cw; /* window col pos */
+            ch = m->wh * (c->cfact / rfacts);
+            /*rfacts -= c->cfact;*/
+            cy = tyr;
         } else {
             row = lrow;
             cx = m->wx; /* window col pos */
+            ch = m->wh * (c->cfact / lfacts);
+            /*lfacts -= c->cfact;*/
+            cy = tyl;
         }
-		ch = m->wh/row;
-		cy = m->wy + rn*ch;
+
+		/*cy = m->wy + rn*ch;*/
                               /* for border on either side */
 		resize(c, cx, cy, cw - 2 * c->bw, ch - 2 * c->bw, False);
+
+        if(cn) {
+            tyr += ch;
+        } else {
+            tyl += ch;
+        }
+
+
 		rn++;
         
         /* check if either column is full, and reset rn */
